a5158758b32c6384a689dc2d8e291c32c58b8c0c
TianHongZXY <zhuxinyu886@163.com>
2022-11-28 21:05:49 +0800

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,255 +1,189 @@
 # ====================================================
-#   Copyright (C) 2021  All rights reserved.
+#   Copyright (C) 2022  All rights reserved.
 #
 #   Author        : Xinyu Zhu
 #   Email         : zhuxy21@mails.tsinghua.edu.cn
 #   File Name     : base_model.py
-#   Last Modified : 2021-11-13 00:58
+#   Last Modified : 2022-07-15 15:48
 #   Describe      : 
 #
 # ====================================================
-
-
+import os
 import argparse
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import wandb
+import numpy as np
+from transformers.optimization import AdamW, get_scheduler
 import pytorch_lightning as pl
-from transformers import AutoModel, AutoTokenizer, AutoConfig, AdamW
-from transformers.optimization import get_linear_schedule_with_warmup
-from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
-from sklearn.metrics import precision_score, recall_score, f1_score
-from torchsnooper import snoop
-from adversarial_loss import AdversarialLoss
-import time
-
-
-class Pooler(pl.LightningModule):
-    """
-    Parameter-free poolers to get the sentence embedding
-    'cls': [CLS] representation with BERT/RoBERTa's MLP pooler.
-    'cls_before_pooler': [CLS] representation without the original MLP pooler.
-    'avg': average of the last layers' hidden states at each token.
-    'avg_top2': average of the last two layers.
-    'avg_first_last': average of the first and the last layers.
-    """
-    def __init__(self, pooler_type):
-        super().__init__()
-        self.pooler_type = pooler_type
-        assert self.pooler_type in ["cls", "cls_before_pooler", "avg", "avg_top2", "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
-
-    def forward(self, attention_mask, outputs):
-        #  use this if pretrained_model.forward(return_dict=True)
-        last_hidden = outputs.last_hidden_state
-        hidden_states = outputs.hidden_states
-
-        #  use this if pretrained_model.forward(return_dict=False)
-        #  last_hidden = outputs[0]
-        #  hidden_states = outputs[2]
-
-        if self.pooler_type in ['cls_before_pooler', 'cls']:
-            return last_hidden[:, 0]
-        elif self.pooler_type == "avg":
-            return ((last_hidden * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1))
-        elif self.pooler_type == "avg_first_last":
-            first_hidden = hidden_states[0]
-            last_hidden = hidden_states[-1]
-            pooled_result = ((first_hidden + last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
-            return pooled_result
-        elif self.pooler_type == "avg_top2":
-            second_last_hidden = hidden_states[-2]
-            last_hidden = hidden_states[-1]
-            pooled_result = ((last_hidden + second_last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
-            return pooled_result
-        else:
-            raise NotImplementedError
-
-
-class MLPLayer(pl.LightningModule):
-    """
-    Head for getting sentence representations over RoBERTa/BERT's CLS representation.
-    Same way as CLIP-Adapter, except dropout is added
-    """
-
-    def __init__(self, hidden_size, alpha=0.5, dropout_rate=0.5):
-        super().__init__()
-        self.alpha = alpha
-        self.dropout = nn.Dropout(p=dropout_rate)
-        self.proj = nn.Sequential(nn.Linear(hidden_size, hidden_size), 
-                                   nn.ReLU(), 
-                                   nn.Linear(hidden_size, hidden_size),
-                                   )
-
-    def forward(self, features):
-        x = self.alpha * self.proj(features) + (1 - self.alpha) * features
-        x = self.dropout(x)
-
-        return x
-
-
-class OutputLayer(pl.LightningModule):
-    def __init__(self, hidden_size, output_size):
-        super().__init__()
-        self.proj = nn.Linear(hidden_size, output_size, bias=False)
-
-    def forward(self, features):
-        logits = self.proj(features)
-
-        return logits
+from pytorch_lightning.strategies import DeepSpeedStrategy
+from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
+from typing import List, Union, Tuple, Optional, Dict
 
 
 class BaseModel(pl.LightningModule):
     """
-    Define training_step, validation_step, optimizer and other general training setting here
+    initiates a PyTorch Lightning base model, defines initialization procedure
     """
     @staticmethod
-    def add_model_specific_args(parent_args):
-        parser = parent_args.add_argument_group('BaseModel')
-        
-        # * Args for general setting
-        parser.add_argument('--num_threads', default=8, type=int)
-        parser.add_argument('--eval', action='store_true', default=False)
-        parser.add_argument('--checkpoint_path', default=None, type=str)
-        parser.add_argument('--seed', default=20020206, type=int)
-        parser.add_argument('--save_dir', default='./save', type=str)
-        parser.add_argument('--model_name', default='ModelName', type=str)
+    def add_model_specific_args(parent_parser):
+        """
+        Add model specific args
+        Returns:
+            parent_parser
+        """
+        parser = parent_parser.add_argument_group('BaseModel')
+        # * Args for model setting
+        parser.add_argument('--seed', default=None, type=int)
         parser.add_argument('--lr', default=1e-5, type=float)
         parser.add_argument('--l2', default=0., type=float)
+        parser.add_argument('--adam_beta1', default=0.9, type=float)
+        parser.add_argument('--adam_beta2', default=0.999, type=float)
+        parser.add_argument('--scheduler', default="linear", type=str)
         parser.add_argument('--warmup', default=0.1, type=float)
-        parser.add_argument('--adv', action='store_true', default=False, help="if use adversarial training")
-        parser.add_argument('--divergence', default='js', type=str)
-        parser.add_argument('--adv_nloop', default=1, type=int,
-                            help="1 (default), inner loop for getting the best perturbations.")
-        parser.add_argument('--adv_step_size', default=1e-3, type=float,
-                            help="1 (default), perturbation size for adversarial training.")
-        parser.add_argument('--adv_alpha', default=1, type=float,
-                            help="1 (default), trade off parameter for adversarial training.")
-        parser.add_argument('--noise_var', default=1e-5, type=float)
-        parser.add_argument('--noise_gamma', default=1e-6, type=float, help="1e-4 (default), eps for adversarial copy training.")
-        parser.add_argument('--project_norm_type', default='inf', type=str)
+        parser.add_argument('--show_training_ex', default=-1, type=int, help="print the training examples for the batch idx. Set to -1 to disable")
+        #TODO Add t5 type, encoder-decoder
+        parser.add_argument('--model_type', default=None, choices=["gpt", "bert"], type=str, help="model type, transformer encoder or decoder")
+        parser.add_argument('--model_name', default=None, type=str, help="path to directory containing huggingface model or huggingface model name")
+        parser.add_argument('--continue_train_from_ckpt', default=None, type=str, help="load a saved lightning checkpoint and continue training")
 
-        return parent_args
+        return parent_parser
 
-    def __init__(self, args, tokenizer):
+    def __init__(self, args):
         super().__init__()
 
         if isinstance(args, dict):
             args = argparse.Namespace(**args)
-
         self.args = args
         self.save_hyperparameters(args)
-        self.tokenizer = tokenizer
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.adv = args.adv
-        if args.adv:
-            self.adv_loss_fn = AdversarialLoss(args)
-        # Set number of different labels here or add it to args
-        self.nlabels = 10
+        # self._consumed_samples = 0
+        # self._consumed_tokens = 0
 
-    def setup(self, stage) -> None:
+    def setup(self, stage):
         if stage == 'fit':
-            train_loader = self.train_dataloader()
-            num_gpus = self.trainer.gpus if self.trainer.gpus is not None else 0
-            self.total_step = int(self.trainer.max_epochs * len(train_loader) / \
-                (max(1, num_gpus) * self.trainer.accumulate_grad_batches))
+            train_loader = self.trainer._data_connector._train_dataloader_source.dataloader()
+
+            # Calculate total steps
+            if self.trainer.max_epochs > 0:
+                world_size = self.trainer.world_size
+                tb_size = self.hparams.micro_batch_size * max(1, world_size)
+                ab_size = self.trainer.accumulate_grad_batches
+                print(f"Training batch size: {tb_size * ab_size}")
+                wandb.config.update({"train_batch_size": tb_size * ab_size})
+                self.total_step = (len(train_loader.dataset) *
+                                    self.trainer.max_epochs // tb_size) // ab_size
+                wandb.config.update({"total_step": self.total_step})
+            else:
+                self.total_step = self.trainer.max_steps // self.trainer.accumulate_grad_batches
+
             print('Total training step:', self.total_step)
+        else:
+            self.total_step = 0
+            print('Not in fit stage, set total training step = 0')
 
-    def train_inputs(self, batch):
-        #  Filter reduntant information(for example: 'sentence') that will be passed to model.forward()
-        inputs = {
-            'input_ids': batch['input_ids'],
-            'attention_mask': batch['attention_mask'],
-            'token_type_ids': batch['token_type_ids']
-        }
-        return inputs 
+    def get_inputs(self, batch) -> Dict:
+        """ process a batch of data to be fed into model, the returned dict should be able to sent to model directly. """
+        raise NotImplementedError()
 
-    def training_step(self, batch, batch_idx):
-        inputs = self.train_inputs(batch)
-        labels = batch['labels']
-        logits = self(**inputs)
+    def training_step(self, batch, batch_idx) -> Dict:
+        """ training step """
+        inputs = self.get_inputs(batch)
+        batch_size = inputs["input_ids"].size(0)
+        loss, logits = self(**inputs)
 
-        if labels is not None:
-            loss = self.loss_fn(logits, labels.view(-1))
+        self.log("train_loss", loss, prog_bar=True, logger=True, on_step=True, batch_size=batch_size)
+        wandb.log({"train_loss": loss})
 
-        if self.adv:
-            adv_loss = self.adv_forward(logits=logits, train_inputs=inputs)
+        return {"loss": loss}
 
-        ntotal = logits.size(0)
-        ncorrect = (logits.argmax(dim=-1) == batch['labels']).long().sum()
-        acc = ncorrect / ntotal
+    def training_epoch_end(self, training_step_outputs):
+        """ save a huggingface checkpoint for each epoch """
+        self.avg_train_loss = np.round(
+            torch.mean(torch.stack([x["loss"] for x in training_step_outputs])).item(), 4)
+        self.log("avg_train_loss", self.avg_train_loss, prog_bar=False, logger=True, on_epoch=True)
+        wandb.log({"avg_train_loss": self.avg_train_loss})
 
-        self.log('train_loss', loss, on_step=True, prog_bar=True)
-        self.log("train_acc", acc, on_step=True, prog_bar=True)
-        if self.adv:
-            self.log('adv_loss', adv_loss, on_step=True, prog_bar=True)
-            return loss + adv_loss
+        self.save_hf_checkpoint()
 
-        return loss
-    
-    def validation_step(self, batch, batch_idx):
-        inputs = self.train_inputs(batch)
-        labels = batch['labels']
-        logits = self(**inputs)
+    def validation_step(self, batch, batch_idx) -> Dict:
+        """ validation step """
+        inputs = self.get_inputs(batch)
+        batch_size = inputs["input_ids"].size(0)
+        loss, logits = self(**inputs)
 
-        predict = logits.argmax(dim=-1).cpu().tolist()
+        self.log("val_loss", loss, prog_bar=False, logger=True, on_step=True, batch_size=batch_size)
+        wandb.log({"val_loss": loss})
 
-        if labels is not None:
-            loss = self.loss_fn(logits, labels.view(-1))
-
-        ntotal = logits.size(0)
-        ncorrect = int((logits.argmax(dim=-1) == batch['labels']).long().sum())
-        acc = ncorrect / ntotal
-
-        self.log('valid_loss', loss, on_step=True, prog_bar=True)
-        self.log("valid_acc", acc, on_step=True, prog_bar=True)
-
-        return ncorrect, ntotal, predict, labels.cpu().tolist()
+        return {"loss": loss}
 
     def validation_epoch_end(self, validation_step_outputs):
-        ncorrect = 0
-        ntotal = 0
-        predictions = []
-        labels = []
-        for x in validation_step_outputs:
-            ncorrect += x[0]
-            ntotal += x[1]
-            predictions += x[2]
-            labels += x[3]
+        self.avg_val_loss = np.round(
+            torch.mean(torch.stack([x["loss"] for x in validation_step_outputs])).item(), 4)
+        self.log("avg_val_loss", self.avg_val_loss, prog_bar=False, logger=True, on_epoch=True)
+        wandb.log({"avg_val_loss": self.avg_val_loss})
 
-        recall = recall_score(labels, predictions, average="macro")
-        precision = precision_score(labels, predictions, average="macro")
-        f1 = f1_score(labels, predictions, average='macro')
-        self.log('valid_acc_epoch', ncorrect / ntotal, on_epoch=True, prog_bar=True)
-        self.log('valid_recall', recall, on_epoch=True, prog_bar=True)
-        self.log('valid_precision', precision, on_epoch=True, prog_bar=True)
-        self.log('valid_f1', f1, on_epoch=True, prog_bar=True)
+    def test_step(self, batch, batch_idx) -> Dict:
+        """ test step """
+        inputs = self.get_inputs(batch)
+        batch_size = inputs["input_ids"].size(0)
+        loss, logits = self(**inputs)
 
-        print("ncorrect = {}, ntotal = {}".format(ncorrect, ntotal))
-        print(f"Validation Accuracy: {round(ncorrect / ntotal, 3)}")
-        print('Validation P: {:.3%}, R: {:.3%}, F1: {:.3%}'.format(precision, recall, f1))
+        self.log("test_loss", loss, prog_bar=False, logger=True, on_step=True, batch_size=batch_size)
+        wandb.log({"test_loss": loss})
 
-    def configure_optimizers(self):
+        return {"loss": loss}
+
+    def test_epoch_end(self, test_step_outputs):
+        self.avg_test_loss = np.round(
+            torch.mean(torch.stack([x["loss"] for x in test_step_outputs])).item(), 4)
+        self.log("avg_test_loss", self.avg_test_loss, prog_bar=False, logger=True, on_epoch=True)
+        wandb.log({"avg_test_loss": self.avg_test_loss})
+
+    def configure_optimizers(self) -> Dict:
         no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        paras = list(filter(lambda p: p[1].requires_grad, self.named_parameters()))
-        paras = [{
-            'params':
-            [p for n, p in paras if not any(nd in n for nd in no_decay)],
-            'weight_decay': self.hparams.l2
-        }, {
-            'params': [p for n, p in paras if any(nd in n for nd in no_decay)],
-            'weight_decay': 0.0
-        }]
-        optimizer = AdamW(paras, lr=self.hparams.lr)
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer, int(self.total_step * self.hparams.warmup),
-            self.total_step)
+        optimizer_grouped_params = [
+            {'params': [p for n, p in self.named_parameters() if not any(
+                nd in n for nd in no_decay)], 'weight_decay': self.hparams.l2},
+            {'params': [p for n, p in self.named_parameters() if any(
+                nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        # Configure optimizer.
+        if isinstance(self.trainer.strategy, DeepSpeedStrategy):
+            if 'offload_optimizer' in self.trainer.training_type_plugin.config['zero_optimization']:
+                optimizer = DeepSpeedCPUAdam(
+                    optimizer_grouped_params, adamw_mode=True,
+                    lr=self.hparams.lr,
+                    betas=(self.hparams.adam_beta1, self.hparams.adam_beta2),
+                    #  eps=self.hparams.adam_epsilon,
+                    )
+            else:
+                optimizer = FusedAdam(
+                    optimizer_grouped_params, adam_w_mode=True,
+                    lr=self.hparams.lr,
+                    betas=(self.hparams.adam_beta1, self.hparams.adam_beta2),
+                    #  eps=self.hparams.adam_epsilon,
+                    )
+        else:
+            optimizer = AdamW(optimizer_grouped_params, lr=self.hparams.lr,
+                              betas=(self.hparams.adam_beta1, self.hparams.adam_beta2),
+                              #  eps=self.hparams.adam_epsilon,
+                              )
+        # Configure learning rate scheduler.
+        warmup_steps = self.hparams.warmup * self.total_step
+        scheduler = get_scheduler(name=self.hparams.scheduler, optimizer=optimizer,
+                                  num_warmup_steps=warmup_steps, num_training_steps=self.total_step)
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
 
         return [{
             'optimizer': optimizer,
-            'lr_scheduler': {
-                'scheduler': scheduler,
-                'interval': 'step',
-                'frequency': 1
-            }
+            'lr_scheduler': scheduler,
         }]
 
+    def save_hf_checkpoint(self) -> None:
+        #  if self.trainer._accelerator_connector.cluster_environment.global_rank() == 0:
+        """Save huggingface model checkpoint and tokenizer"""
+        if self.global_rank == 0:
+            save_path = os.path.join(
+                self.trainer.checkpoint_callback.dirpath if self.trainer else self.hparams.save_dir,
+                'hf_pretrained_epoch{}_step{}'.format(self.current_epoch, self.global_step))
+            self.model.save_pretrained(save_path)
+            self.tokenizer.save_pretrained(save_path)
---
+++ models/utils.py
@@ -0,0 +1,42 @@
+import pytorch_lightning as pl
+
+
+class Pooler(pl.LightningModule):
+    """
+    Parameter-free poolers to get the sentence embedding
+    'cls': [CLS] representation with BERT/RoBERTa's MLP pooler.
+    'cls_before_pooler': [CLS] representation without the original MLP pooler.
+    'avg': average of the last layers' hidden states at each token.
+    'avg_top2': average of the last two layers.
+    'avg_first_last': average of the first and the last layers.
+    """
+    def __init__(self, pooler_type):
+        super().__init__()
+        self.pooler_type = pooler_type
+        assert self.pooler_type in ["cls", "cls_before_pooler", "avg", "avg_top2", "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
+
+    def forward(self, attention_mask, outputs):
+        #  use this if pretrained_model.forward(return_dict=True)
+        last_hidden = outputs.last_hidden_state
+        hidden_states = outputs.hidden_states
+
+        #  use this if pretrained_model.forward(return_dict=False)
+        #  last_hidden = outputs[0]
+        #  hidden_states = outputs[2]
+
+        if self.pooler_type in ['cls_before_pooler', 'cls']:
+            return last_hidden[:, 0]
+        elif self.pooler_type == "avg":
+            return ((last_hidden * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1))
+        elif self.pooler_type == "avg_first_last":
+            first_hidden = hidden_states[0]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((first_hidden + last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        elif self.pooler_type == "avg_top2":
+            second_last_hidden = hidden_states[-2]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((last_hidden + second_last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        else:
+            raise NotImplementedError
